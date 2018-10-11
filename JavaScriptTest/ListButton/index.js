0a53552a2711caf865c495bc5b8c2cef7913cefd
someone <someone@someplace.com>
2018-10-11 09:41:57 -0500

    list button

--- JavaScriptTest/ListButton/index.js
+++ JavaScriptTest/ListButton/index.js
@@ -5,4 +5,14 @@ window.onload = () => {
     setTextButton.onclick = () => {
         userInput.value = 'The first item for my list';
      }
+
+     const listButton = document.getElementById('add-to-list'); 
+     const setTextButton = document.getElementById('set-text');
+const userInput = document.getElementById('list-data');
+
+
+listButton.onclick = () => {
+    const myList = document.getElementById('my-list');
+    elfCode.appendToList(myList, userInput.value);
+}
 }
