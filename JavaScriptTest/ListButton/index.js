10eff1b8944597dddebe8018bd5baa4ec6ced507
someone <someone@someplace.com>
2018-10-14 01:37:05 -0500

    Adding ListButton file

--- JavaScriptTest/ListButton/index.js
+++ JavaScriptTest/ListButton/index.js
@@ -1,16 +1,15 @@
 window.onload = () => {
+ // Part I here
     const userInput = document.getElementById('list-data');
     const setTextButton = document.getElementById('set-text');
 
-    setTextButton.onclick = () => {
-        userInput.value = 'The first item for my list';
      }
+// Part II here
+     
+    setTextButton.onclick = () => {
+        userInput.value = 'The first item for my list';}
 
-     const listButton = document.getElementById('add-to-list'); 
-     const setTextButton = document.getElementById('set-text');
-const userInput = document.getElementById('list-data');
-
-
+// Part III here
 listButton.onclick = () => {
     const myList = document.getElementById('my-list');
     elfCode.appendToList(myList, userInput.value);
