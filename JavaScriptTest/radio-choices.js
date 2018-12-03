913eeac42fe116ad3039a97887aac779ec7fda92
someone <someone@someplace.com>
2018-12-02 19:29:39 -0800

    adding more colors

--- JavaScriptTest/radio-choices.js
+++ JavaScriptTest/radio-choices.js
@@ -1,24 +1,16 @@
 window.onload = () => {
-        const myForm = document.getElementById('sort-type');
+
+    const myForm = document.getElementById('sort-type');
 
     myForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const value = document.querySelector('input[name="sorter"]:checked').value;
-        //const checkedSorterRadio = document.getElementById('input[name="sorter"]:checked');
-        //const value = checkSorterRadio.value;
-        if (value.toLowerCase() === 'language') {
+        if (value.toLowerCase() === 'green') {
+            setColor("blueBackground");
             showChoice(value);
         } else {
+            setColor("greenBackground");
             showChoice(value);
         }
-    });
-    
-}
-
-function showChoice(value) {
-
-    console.log(value);
-    const userDisplay = document.getElementById('user-choice');
-    userDisplay.textcontent = value;
-
+    }
 }
